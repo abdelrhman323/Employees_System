291343718d5f4143eff46af0b09a23ca5a965211
Abdelrhman Adel <abduadell37@gmail.com>
2025-10-04 19:19:26 +0300

    divid the system to classes and it's under testing now

---
+++ employee_manager.py
@@ -0,0 +1,36 @@
+# Employees_System
+from employees import Employee
+
+
+class EmployeesManager:
+    def __init__(self):
+        self.data = []
+
+    def add_new_employee(self,name,age,salary):
+        emp = Employee(name,age,salary)
+        self.data.append(emp.get_employee_data())
+
+    def list_all_employees(self):
+        if self.data: 
+            for idx,employee in enumerate(self.data):
+                print(f"Employee name: {employee[0]} has age: {employee[1]} and salary: {employee[2]}") 
+        else:
+            print("No employees at this moment")
+
+    def delete_by_age(self,start_age , end_age):
+        self.data = [employee for employee in self.data if not int(start_age) <= employee[1] <= int(end_age)]
+
+    def update_salary_by_name(self,name):
+        for idx,employee in enumerate(self.data):
+            if name == employee[0]:
+                salary_update = float(input("Enter the new salary: "))
+                self.data[idx] = (employee[0],employee[1],salary_update)
+                print(f"Salary of {employee[0]} updated to {salary_update}")
+                break
+        else:
+            print("No employee with such a name")       
+    def end_program(self):
+        print("Thank you for using our system")
+        exit(1) 
+
+
--- main.py
+++ main.py
@@ -1,39 +1,14 @@
 # Employees_System
 
-def print_dachbord():
-    msgs = ["Programe options:","1) Add a new employee","2) List all employees",
-    "3) Delete by age range","4) Update salary given a name","5) End the program"]
-    print('\n'.join(item for item in msgs)
-    )
-def choice_valid(choice):
-    return 0 < choice <= 5
-def add_new_employee():
-    pass
-def list_all_employees():
-    pass
-def delete_by_age():
-    pass    
-def update_salary_by_name():
-    pass 
-def end_program():
-    print("Thank you for using our system")
-    exit(1)       
+from front_end_manager import FrontendManager as FE
+frontend = FE() 
 
 if __name__ == "__main__":    
     while True:
-        print_dachbord()
-        choice = int(input("Enter your choice (from 1 to 5): "))
-        if not choice_valid(choice):
+        FE.print_dachbord()
+        choice = input("Enter your choice (from 1 to 5): ")
+        if not frontend.choice_valid(choice):
             print("Invalid range. Try again!")
             continue
-        else:
-            if choice == 1:
-                add_new_employee()
-            elif choice == 2:
-                list_all_employees()    
-            elif choice == 3:
-                delete_by_age()
-            elif choice == 4:
-                update_salary_by_name()
-            elif choice == 5:
-                end_program()
+        else:        
+            frontend.show_menu(choice)
